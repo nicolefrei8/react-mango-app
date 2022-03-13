28e98c6ca5e1c7e28dfb25bb86a9621b84e13abf
nicolefrei8 <89259126+nicolefrei8@users.noreply.github.com>
2022-03-13 16:25:39 +1030

    weather icon integration with api

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       city: response.data.name,
       description: response.data.weather[0].description,
-      weatherIcon: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      weatherIcon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
       date: new Date(response.data.dt * 1000),
     });
   }
